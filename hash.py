d55966380c1f71c1b78d164922232dd519e27c5d
Machina <sraabooshki@gmail.com>
2021-02-25 20:49:27 +0200

    Included Read Data

--- hash.py
+++ hash.py
@@ -1,5 +1,23 @@
 def main():
-    print(1)
+    file = "a.txt"
+    with open(file, "r") as f:
+        data = [i.strip() for i in f.readlines()]
+        #print(data)
+        k = 0
+        D, I, S, V, F = map(int, data[0].split(' '))
+        for _ in range(S):
+            k += 1
+            line = data[k]
+            B, E, NAME, L = line.split(' ')
+            B, E, L = int(B), int(E), int(L)
+
+        for i in range(V):
+            k += 1
+            line = data[k]
+            P = int(line[0])
+            STREETS = line.split(' ')[1:]
+            print(P, STREETS)
+            
 
 
 if __name__ == "__main__":
