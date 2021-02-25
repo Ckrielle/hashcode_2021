c6a5aa05f15715985a1c0bceae0300a485874518
Machina <sraabooshki@gmail.com>
2021-02-25 21:04:47 +0200

    Fixed Grouping

--- hash.py
+++ hash.py
@@ -1,10 +1,3 @@
-
-def path_time(streets):
-    path_time = 0
-    for street in streets:
-        path_time += street[3]
-    return path_time
-
 def main():
     file = "a.txt"
     with open(file, "r") as f:
@@ -12,18 +5,29 @@ def main():
         #print(data)
         k = 0
         D, I, S, V, F = map(int, data[0].split(' '))
+        STREETS = {}
+        NAMES = []
         for _ in range(S):
             k += 1
             line = data[k]
             B, E, NAME, L = line.split(' ')
             B, E, L = int(B), int(E), int(L)
-
+            STREETS[NAME] = {
+                        'BEGIN': B,
+                        'END': E,
+                        'LENGTH': L
+                    }
+            NAMES.append(NAME)
+        print(STREETS, NAMES)
+                        
+        CARS = []
         for i in range(V):
             k += 1
             line = data[k]
             P = int(line[0])
-            STREETS = line.split(' ')[1:]
-            print(P, STREETS)
+            CAR_STREETS = line.split(' ')[1:]
+            CARS.append(CAR_STREETS)
+        print(CARS)
             
 
 
